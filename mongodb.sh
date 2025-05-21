e02cc15d901ecec48161624c2e3b2a801f56a437
durgababu <durgababu.nallagachu@gmail.com>
2025-05-21 13:10:55 +0530

    starting mongodb server

--- mongodb.sh
+++ mongodb.sh
@@ -5,43 +5,43 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/logs/roboshops-logs"
-SCRIPT_NAME=$(echo "$0" | cut -d "." -f1) 
+SCRIPT_NAME=$(basename "$0" | cut -d "." -f1) 
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p "$LOGS_FOLDER"
-echo "script started time at : $(date)" &>> | tee -a $LOG_FILE
+echo "script started time at : $(date)" | tee -a |  tee -a $LOG_FILE
+
 if [ "$USERID" -ne 0 ]; then
-    echo -e "$R ERROR:: Please run this script with root access $N" &>> | tee -a $LOG_FILE
-    exit 1 # Give other than 0 up to 127
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a |  tee -a $LOG_FILE
+    exit 1
 else
-    echo "You are running with root access" &>> | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a |  tee -a $LOG_FILE
 fi
+
 # Validate function takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ "$1" -eq 0 ]; then
-        echo -e "Installing $2 is ... $G SUCCESS $N" &>> | tee -a $LOG_FILE
+        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a |  tee -a $LOG_FILE
     else
-        echo -e "Installing $2 is ... $R FAILURE $N" &>> | tee -a $LOG_FILE
+        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a |  tee -a $LOG_FILE
         exit 1
     fi
 }
 
- cp mongo.repo /etc/yum.repos.d/mongodb.repo &>> | tee -a $LOG_FILE
- dnf install mongodb-org -y 
- 
- VALIDATE $? "Installing mongodb server"
-    systemctl enable mongod 
-    VALIDATE $? "enabling mongodb"
+cp mongo.repo /etc/yum.repos.d/mongodb.repo &>> |  tee -a $LOG_FILE
+dnf install mongodb-org -y &>> |  tee -a $LOG_FILE
 
- 
-    systemctl start mongod
-    VALIDATE $? "Starting mongogb"
+VALIDATE $? "Installing mongodb server"
+systemctl enable mongod &>> |  tee -a $LOG_FILE
+VALIDATE $? "Enabling mongodb"
 
-sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+systemctl start mongod &>> |  tee -a $LOG_FILE
+VALIDATE $? "Starting mongodb"
 
-VALIDATE $? "Editing monogdb config file  for remote access to other server "
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+VALIDATE $? "Editing mongodb config file for remote access"
 
-    systemctl restart mongod
-    VALIDATE $? "Restarting mongodb"
- 
-    echo -e "$G MongoDB is installed and running successfully $N" &>> | tee -a $LOG_FILE
+systemctl restart mongod &>> |  tee -a $LOG_FILE
+VALIDATE $? "Restarting mongodb"
+
+echo -e "$G MongoDB is installed and running successfully $N" | tee -a |  tee -a $LOG_FILE
