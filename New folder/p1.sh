89ae8109cf65dc0214ffbc2392a0dd976d831b29
durgababu <durgababu.nallagachu@gmail.com>
2025-05-24 07:39:34 +0530

    learining shell

--- New folder/p1.sh
+++ New folder/p1.sh
@@ -1,1 +1,10 @@
-echo "Hello, $name! You have $count messages."
+#!/bin/bash
+read -p "Enter a number: " num
+
+if [ "$num" -gt 10 ]; then
+    echo "Number is greater than 10"
+elif [ "$num" -eq 10 ]; then
+    echo "Number is exactly 10"
+else
+    echo "Number is less than 10"
+fi
