bca70cfe9679979fffbd2d9b8f25754d11bdb0e3
durgababu <durgababu.nallagachu@gmail.com>
2025-05-24 06:48:21 +0530

    A

--- New folder/p1.sh
+++ New folder/p1.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "enter your name as per format"
+echo "Enter your name as per format:"
 read name
-echo "It's  yes yournmae man do dit that awsome bro yoyo honey singo $name!"
-xfvx
-
-
+echo "It's yes, your name is $name! Do it—that's awesome, bro! Yoyo Honey Singh!"
